70206f519eccc727a008dbf5169b4b063976c0ed
Rene <ThatRetGames@gmail.com>
2019-02-04 14:28:03 +0100

    added database connection

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "sgni.nl";
+$username = "6in1 René Tielen";
+$password = "113924";
+
+try {
+   $conn = new PDO("mysql:host=$servername;dbname=6in1 René Tielen", $username, $password);
+   // set the PDO error mode to exception
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "Connected successfully";
+   }
+catch(PDOException $e)
+   {
+   echo "Connection failed: " . $e->getMessage();
+   }
+?>
