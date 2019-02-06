d519887ad0db17a8cd7fc4a88997de2124472dc9
yaboygrasmaaier <47143852+yaboygrasmaaier@users.noreply.github.com>
2019-02-06 15:19:29 +0100

    postcreated

--- postcreated.php
+++ postcreated.php
@@ -55,7 +55,7 @@
                 <a href="#" class="postuser">Username</a>
                 <span class="postdate">dd-mm-yyyy</span>
               </div>
-              <p class="posttext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p class="posttext"><?php echo $_POST["postMessage"]; ?></p>
             </div>
 
         </div>
