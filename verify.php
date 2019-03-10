f1f6ce9575610d2d07cfe18e21c196f4efa3b500
Rene <ThatRetGames@gmail.com>
2019-03-10 13:53:23 +0100

    added email verification

---
+++ verify.php
@@ -0,0 +1,41 @@
+<?php
+/*Verifies registered user email, the link to this page
+  is included in the register.php email message.
+*/
+
+require 'db.php';
+session_start();
+
+// Make sure email and hash variables aren't empty
+if (isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['email'])){
+
+  $email = $db->escape_string($_GET['email']);
+  $hash = $db->escape_string($_GET['hash']);
+
+  //Select user with matching email and hash, who hasn't verified their account yet (active = 0)
+  $stmt = $db->query("SELECT * FROM User WHERE email='$email' AND Hash='$hash' AND Active='0'");
+  $result = $stmt->fetch();
+
+    if ( $result->num_rows == 0 ){
+      $_SESSION['message'] = "Account has already been activated or the URL is invalid!";
+      header("location: error.php");
+
+    }
+    else {
+      $_SESSION['message'] = "Your account has been activated!";
+
+      // Set the user status to active (active = 1)
+      $stmt = $db->query("UPDATE user SET Active='1' WHERE email='$email'");
+      $stmt->execute() or die($db->error); // misschien vervangen door $e
+
+      $_SESSION['active'] = 1;
+
+      header("location: success.php")
+    }
+  else{
+    $_SESSION['message'] = "Invalid parameters provided for account verification!";
+    header("location: error.php")
+  }
+
+}
+ ?>
