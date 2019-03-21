7ac1672aa1dad2d195d6d462f423bc79c18bd7a8
yaboygrasmaaier <47143852+yaboygrasmaaier@users.noreply.github.com>
2019-03-21 20:54:13 +0100

    added general feed

---
+++ feed_general.php
@@ -0,0 +1,124 @@
+<?php
+require 'db.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ReadIt - Home</title>
+
+    <!-- CSS Link -->
+    <link rel="stylesheet" type="text/css" href="readitstyle.css">
+  </head>
+  <body>
+    <div class="header">
+
+      <div class="logo">
+        <img src="Images/logo.png" alt="ReadIt Logo">
+        <h3>ReadIt</h3>
+      </div>
+
+      <div class="navbar navhover">
+        <a href="index.php">Home</a>
+        <a href="#">Forums</a>
+        <a href="topfeed.php">Top</a>
+      </div>
+
+      <div class="accountbar">
+        <div class="accountbarplaceholder"></div>
+        <div class="dropdown navhover">
+          <button class="dropbtn">Account</button>
+          <div class="dropdown-content">
+            <?php
+            if($_SESSION['logged_in']==true){
+              echo '<a href="logout.php">Logout</a>
+              <a href="profile.php">Profile</a>
+              <a href="post_creation_form.php">Post Creation</a>';
+            }
+            else{
+              echo '<a href="login_form.php">Login</a>
+              <a href="register_form.php">Sign Up</a>';
+            }
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Content -->
+
+    <div class="content">
+
+      <!-- Page indication-->
+      <div class="top">
+        <div class="between7-5"></div>
+
+        <div class="maintop">
+          <p>Home</p>
+
+        </div>
+
+        <div class="between7-5"></div>
+      </div>
+
+      <!-- Main content (posts etc.)-->
+
+      <div class="mid">
+        <!--Flex space filler-->
+        <div class="between7-5"></div>
+
+        <!-- Main feed-->
+        <div class="main">
+          <?php
+          $stmt = $db->prepare("SELECT Post.Id, Post.Title, Post.Message, Post.Datum, User.Username FROM Post,User WHERE Post.UserId=User.Id ORDER BY Datum DESC LIMIT 20");
+          $stmt->execute();
+          $result = $stmt->fetch(PDO::FETCH_ASSOC);
+          for ($i = 0; $i <= 19; $i++) {
+          echo $result[0]['Post.Title'];
+/*
+          <div class='post'>
+              <div class='postheader'>
+                <a href='#' class='posttitle'><b>".$result[0][0]."</b></a>
+                <a href='#' class='postuser'>Username</a>
+                <span class='postdate'>dd-mm-yyyy</span>
+              </div>
+              <p class='posttext'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+*/
+
+          }
+         ?>
+        </div>
+
+        <!--Flex space filler-->
+        <div class="between5"></div>
+
+        <!-- Sidebar content-->
+        <div class="sidebar">
+          <div class="sidebar-post">
+            <p class="sidebar-post-title">Top forums</p>
+            <p class="sidebar-post-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+
+        <!--Flex space filler-->
+        <div class="between7-5"></div>
+
+      </div>
+
+    <!-- Buttons for next X posts go here-->
+    <div class="bottom">
+
+    </div>
+
+
+    </div>
+
+
+    <!-- Footer -->
+
+  </body>
+</html>
